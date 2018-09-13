bfe00873e652daec94c1333c64561f66e0238ea1
wuhengliangliang <252856663@qq.com>
2018-09-13 14:10:58 +0800

    Initial commit

---
+++ js/new_file.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+	<script>
+	console.log(true+1);
+	console.log(false+1);
+	
+	
+	
+	</script>
+	
+	
+	</body>
+</html>
